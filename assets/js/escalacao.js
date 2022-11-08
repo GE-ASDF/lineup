5b26fd3b0d0ceee2c40bd4606ecfe0adaf181362
Anderson Souza <andersonsouza007@live.com>
2022-11-08 11:55:07 -0300

    First commit

---
+++ assets/js/escalacao.js
@@ -0,0 +1,17 @@
+const escalacao = [
+    {
+        camisa: "22",
+        nome: "Bolsonaro",
+        posicao:"Presidente",
+    },
+    {
+        camisa: "13",
+        nome: "Lula",
+        posicao:"Detento",
+    },
+    {
+        camisa: "10",
+        nome: "Tarcísio",
+        posicao:"Ministro do Desenvolvimento",
+    },
+]
